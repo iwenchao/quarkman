9632fa8c9bbed1f763fb903f780dc61f5c56ed6b
iwen <iwenchaos@gmail.com>
2019-07-26 08:08:24 +0800

    更新app icon

---
+++ app/src/main/java/cn/iwenchaos/quarkman/base/QuarkApp.java
@@ -0,0 +1,21 @@
+package cn.iwenchaos.quarkman.base;
+
+import android.app.Application;
+
+/**
+ * Created by chaos
+ * on 2019/7/26. 07:58
+ * 文件描述：夸克man的入口
+ *
+ *
+ */
+public class QuarkApp extends Application {
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
